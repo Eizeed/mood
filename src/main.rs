935e2408fd749020cb741a09de9359342eade941
Eizeed <eizeed321@gmail.com>
2025-08-05 12:30:47 +0500

    resolve: #47

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use crossterm::{ExecutableCommand, event::DisableMouseCapture};
 use ratatui::layout::Rect;
+use std::io::stdout;
 
 use crate::app::Player;
 
+mod action;
 mod app;
+mod config;
 mod input;
 mod io;
-mod music;
-mod widget;
 mod model;
-mod action;
+mod music;
 mod task;
-mod config;
+mod widget;
 
 fn main() {
+    set_panic_hook();
     let terminal = ratatui::init();
 
     let size = terminal.size().unwrap();
@@ -22,4 +25,14 @@ fn main() {
     app.start(terminal);
 
     ratatui::restore();
+}
+
+fn set_panic_hook() {
+    let hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |info| {
+        ratatui::restore();
+        stdout().execute(DisableMouseCapture).unwrap();
+
+        hook(info);
+    }));
 }
