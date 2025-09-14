426fe5ce7d3c3d810283448d38e45dad21a77351
Eizeed <eizeed321@gmail.com>
2025-09-14 15:27:45 +0500

    init

---
+++ src/event.rs
@@ -0,0 +1,83 @@
+use std::time::Duration;
+
+use crossterm::event;
+
+pub struct EventHandler {
+    rx: crossbeam_channel::Receiver<Event>,
+}
+
+impl EventHandler {
+    pub fn new(tickrate: Duration) -> Self {
+        let (event_tx, event_rx) = crossbeam_channel::unbounded();
+
+        tokio::spawn(async move {
+            loop {
+                if event::poll(tickrate).unwrap() {
+                    if let event::Event::Key(key) = event::read().unwrap() {
+                        let key = Key::from(key);
+                        event_tx.send(Event::Input(key)).unwrap();
+                    }
+                }
+
+                event_tx.send(Event::Tick).unwrap();
+            }
+        });
+
+        EventHandler { rx: event_rx }
+    }
+
+    pub fn next(&self) -> Result<Event, crossbeam_channel::RecvError> {
+        self.rx.recv()
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Event {
+    Tick,
+    Input(Key),
+}
+
+#[derive(Copy, Clone)]
+pub enum Key {
+    Enter,
+    Tab,
+    Backspace,
+    Esc,
+
+    Left,
+    Right,
+    Up,
+    Down,
+
+    Ins,
+    Delete,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+
+    F0,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+
+    Char(char),
+    Ctrl(char),
+    Alt(char),
+    Unknown,
+}
+
+impl From<event::KeyEvent> for Key {
+    fn from(value: event::KeyEvent) -> Self {
+        Self::Unknown
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::time::Duration;
+
+use crate::event::{Event, EventHandler};
+
+mod event;
+
+fn main() -> color_eyre::Result<()> {
+    let event_handler = EventHandler::new(Duration::from_millis(250));
+
+    loop {
+        //render
+
+        match event_handler.next()? {
+            Event::Input(key) => {}
+            Event::Tick => (),
+        }
+    }
+}
