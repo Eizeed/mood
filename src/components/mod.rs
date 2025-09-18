d5353bbc6b203901c647805a793da480c5c96151
Eizeed <eizeed321@gmail.com>
2025-09-18 15:56:10 +0500

    feat(component trait): add component trait and re-export WidgetRef trait from ratatui

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,12 @@
-mod tracklist;
-mod playlist;
 mod player_controls;
+mod playlist;
+mod tracklist;
 
-pub use tracklist::TracklistComponent;
-pub use playlist::PlaylistComponent;
 pub use player_controls::PlayerControlsComponent;
+pub use playlist::PlaylistComponent;
+pub use tracklist::TracklistComponent;
+
+pub use ratatui::widgets::WidgetRef;
+pub trait Component {
+    fn event(&mut self, key: crate::event::Key);
+}
