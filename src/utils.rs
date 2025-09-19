1b11ba5675b7038491d3f30f71eb107cfed7ef3f
Eizeed <eizeed321@gmail.com>
2025-09-19 16:41:45 +0500

    feat(sound handler): add custom source, implement event loop for audio thread

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
-use std::time::Duration;
+use std::{io::BufReader, time::Duration};
 
 use color_eyre::Result;
 use crossbeam_channel::{Receiver, Sender};
 use crossterm::event;
+use rodio::Source;
 
 use crate::event::{AudioMessage, Command, Event, Key};
 
@@ -28,9 +29,32 @@ pub fn spawn_audio_thread(
     event_tx: Sender<Event>,
 ) -> color_eyre::Result<()> {
     _ = std::thread::spawn(move || -> Result<()> {
+        let stream_handle =
+            rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
+        let sink = rodio::Sink::connect_new(&stream_handle.mixer());
+        sink.set_volume(0.05);
+
         loop {
             // Accept command
-            command_rx.recv()?;
+            let cmd = command_rx.recv()?;
+
+            match cmd {
+                Command::Play(path) => {
+                    let file = std::fs::File::open(&path).unwrap();
+                    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+                    let notify_source = NotifySource {
+                        inner: source,
+                        main_handle: event_tx.clone(),
+                    };
+
+                    sink.clear();
+                    sink.append(notify_source);
+                    sink.play();
+                }
+                Command::Noop => {
+                    continue;
+                }
+            }
 
             // Emmit event
             event_tx.send(Event::Audio(AudioMessage::Noop))?;
@@ -38,4 +62,44 @@ pub fn spawn_audio_thread(
     });
 
     Ok(())
+}
+
+struct NotifySource<T>
+where
+    T: Source,
+{
+    inner: T,
+    main_handle: Sender<Event>,
+}
+
+impl<T> Iterator for NotifySource<T>
+where
+    T: Source,
+{
+    type Item = T::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl<T> Source for NotifySource<T>
+where
+    T: Source,
+{
+    fn current_span_len(&self) -> Option<usize> {
+        self.inner.current_span_len()
+    }
+
+    fn channels(&self) -> rodio::ChannelCount {
+        self.inner.channels()
+    }
+
+    fn sample_rate(&self) -> rodio::SampleRate {
+        self.inner.sample_rate()
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        self.inner.total_duration()
+    }
 }
